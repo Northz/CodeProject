0c58b3c23fbcf1749fa30fe7586891705dd9d002
Dan Huy Ngoc Nguyen <danhuyngoc@gmail.com>
2017-05-16 14:15:13 +0200

    Add project files.

---
+++ Intermediate/Build/Win64/UE4Editor/Development/CodeProject/PCH.CodeProject.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/danhu/Documents/Unreal Projects/CodeProject/Intermediate/Build/Win64/UE4Editor/Development/CodeProject/PCH.CodeProject.h"
---
+++ Source/CodeProject/CodeProjectGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "CodeProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACodeProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACodeProjectGameMode();
+};
+
+
+
